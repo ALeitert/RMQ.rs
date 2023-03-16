e866f66d670b2e3b04abb2c0cea18d082e10f915
Arne <aleitert@kent.edu>
2023-03-16 20:30:34 +0800

    Limited logarithm to usize.

--- src/log.rs
+++ src/log.rs
@@ -2,30 +2,12 @@
 
 /// Returns the floor of the logarithm base 2 of the given number `n`.
 #[inline(always)]
-pub const fn log_f_32(n: u32) -> u32 {
-    31 ^ (n | 1).leading_zeros()
-}
-
-/// Returns the floor of the logarithm base 2 of the given number `n`.
-#[inline(always)]
-pub const fn log_f_64(n: u64) -> u32 {
-    63 ^ (n | 1).leading_zeros()
-}
-
-/// Returns the floor of the logarithm base 2 of the given number `n`.
-#[inline(always)]
 pub const fn log_f(n: usize) -> usize {
     (8 * std::mem::size_of::<usize>() - 1) ^ (n | 1).leading_zeros() as usize
 }
 
 /// Returns the ceil of the logarithm base 2 of the given number `n`.
 #[inline(always)]
-pub const fn log_c_32(n: u32) -> u32 {
-    log_f_32(n - 1) + 1
-}
-
-/// Returns the ceil of the logarithm base 2 of the given number `n`.
-#[inline(always)]
-pub const fn log_c_64(n: u64) -> u32 {
-    log_f_64(n - 1) + 1
+pub const fn log_c(n: usize) -> usize {
+    log_f(n - 1) + 1
 }
